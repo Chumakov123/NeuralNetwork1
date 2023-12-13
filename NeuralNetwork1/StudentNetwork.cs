d9fc8ddeab01cd682315805b21a5765bf1cc87f6
Chumakov123 <Chumakov225@gmail.com>
2023-12-13 13:44:39 +0300

    fix network

--- NeuralNetwork1/StudentNetwork.cs
+++ NeuralNetwork1/StudentNetwork.cs
@@ -9,88 +9,58 @@ namespace NeuralNetwork1
 {
     public class StudentNetwork : BaseNetwork
     {
+        private static double Sigmoid(double x) => 1.0 / (1.0 + Math.Exp(-x));
+        private static double SigmoidDerivative(double x) => x * (1 - x);
+        public static double learningSpeed = 0.01;
+        private static Random r = new Random();
         private class Neuron
         {
-            //Входной взвешенный сигнал
-            public double charge = 0;
-            //Выходной сигнал
-            public double output = 0;
-            //Ошибка
-            public double error = 0;
-            //Сигнал поляризации (нафига нужен и почему -1)
-            public double biasSignal = 1.0;
-
-            private static readonly Random rnd = new Random();
-            //Диапазон инициализации весов (нужно разобраться почему -1 и 1)
-            private const double initMinWeight = -1;
-            private const double initMaxWeight = 1;
-            //Количество узлов на предыдущем слое (можно избавиться)
-            private readonly int inputLayerSize = 0;
-            //Входные веса нейрона
-            private readonly double[] weights = null;
-            //Вес на сигнале поляризации (разобраться)
-            private double biasWeight = 0.01;
-            //Ссылка на предыдущий слой
-            private readonly Neuron[] inputLayer = null;
-            public Neuron(Neuron[] prevLayerNeurons)
-            {
-                inputLayer = prevLayerNeurons;
-                if (prevLayerNeurons == null || prevLayerNeurons.Length == 0)
-                    return;
-
-                inputLayerSize = prevLayerNeurons.Length;
-                weights = new double[inputLayerSize];
-
-                for (int i = 0; i < weights.Length; ++i)
-                {
-                    weights[i] = initMinWeight + rnd.NextDouble() * (initMaxWeight - initMinWeight);
-                }
-            }
-            //Вычисление пороговой функции (активация нейрона). Для сенсоров не используется
+            public Neuron[] inputs;
+            public double[] weights;
+            public double error;
+            public double biasWeight;
+            public double output;
             public void Activate()
             {
-                //Взвешенное значение сигнала поляризации
-                charge = biasWeight * biasSignal;
-                for (int i = 0; i < inputLayer.Length; ++i)
-                {
-                    charge += inputLayer[i].output * weights[i];
-                }
-                //Считаем выход нейрона
-                output = ActivationFunc(charge);
-                //Сброс входного сигнала
-                charge = 0;
-            }
-            public void BackpropError(double ita)
-            {
-                //Сначала обрабатываем ошибку собственно в текущем нейроне
-                error *= output * (1 - output);
-                //Теперь разбираемся с сигналом поляризации - он имеет выход -1 и вес biasWeight, его пересчитыва
-                biasWeight += ita * error * biasSignal;
+                    double weightedSum = 0;
 
-                //Можно по формуле 2*alpha*out(1-out)*Сумма(ошибка в зависимых x вес)
+                    for (int i = 0; i < inputs.Length; ++i)
+                    {
+                        weightedSum += inputs[i].output * weights[i];
+                    }
+                    weightedSum += biasWeight;
 
-                for (int i = 0; i < inputLayerSize; ++i)
-                {
-                    inputLayer[i].error += error * weights[i];
-                    //Проброс ошибки на предыдущий слой 
-                }
-                for (int i = 0; i < inputLayerSize; ++i)
-                {
-                    weights[i] += ita * error * inputLayer[i].output;
-                }
-                error = 0;
+                    output = Sigmoid(weightedSum);
             }
-            //Функция активации (Сигмоида)
-            private double ActivationFunc(double x)
+            public Neuron(Neuron[] prevLayerNeurons)
             {
-                return 1.0 / (1.0 + Math.Exp(-x));
+                if (prevLayerNeurons == null || prevLayerNeurons.Length == 0)
+                    return;
+                inputs = prevLayerNeurons;
+                weights = new double[inputs.Length];
+                randomizeWeights();
+            }
+            private void randomizeWeights()
+            {
+                for (int i = 0; i < weights.Length; ++i)
+                {
+                    weights[i] = 0.2 * r.NextDouble() - 0.1;
+                }
+                biasWeight = 0.2 * r.NextDouble() - 0.1;
+            }
+            public void AdjustWeights()
+            {
+                for (int i = 0; i < weights.Length; ++i)
+                {
+                    weights[i] -= learningSpeed * error * inputs[i].output;
+                }
+                biasWeight -= learningSpeed * error;
             }
         }
 
-        public double learningSpeed = 0.001; // Скорость обучения
         private Neuron[] sensors;
         private Neuron[] outputs;
-        private Neuron[][] layers; //Здесь создаются нейроны, остальные массивы - ссылки
+        private Neuron[][] layers; //Здесь создаются нейроны, остальные массивы - просто ссылки
 
         private readonly Stopwatch watch = new Stopwatch();
 
@@ -113,7 +83,6 @@ namespace NeuralNetwork1
             layers[0] = new Neuron[structure[0]];
             for (int neuron = 0; neuron < structure[0]; ++neuron)
                 layers[0][neuron] = new Neuron(null);
-            sensors = layers[0];
 
             //Остальные слои, указывая каждому нейрону предыдущий слой
             for (int layer = 1; layer < structure.Length; ++layer)
@@ -122,18 +91,18 @@ namespace NeuralNetwork1
                 for (int neuron = 0; neuron < structure[layer]; ++neuron)
                     layers[layer][neuron] = new Neuron(layers[layer - 1]);
             }
-            //Ссылка на выходной слой
+            //Ссылки на входной и выходной слои
+            sensors = layers[0];
             outputs = layers[layers.Length - 1];
         }
         //Однократный запуск сети
         private double[] Run(Sample image)
         {
             double[] result = Compute(image.input);
-            //Inversion(result);
             image.ProcessPrediction(result);
             return result;
         }
-
+        //Обучение на одном элементе
         public override int Train(Sample sample, double acceptableError, bool parallel)
         {
             int iterations = 0;
@@ -143,25 +112,25 @@ namespace NeuralNetwork1
 
             while (error > acceptableError)
             {
-                Debug.WriteLine($"e {error} a {acceptableError}");
+                //Debug.WriteLine($"e {error} a {acceptableError}");
                 Run(sample);
                 error = sample.EstimatedError();
 
                 ++iterations;
-                BackProp(sample, learningSpeed);
+                BackProp(sample);
             }
             return iterations;
         }
-
+        //Обучение на наборе
         public override double TrainOnDataSet(SamplesSet samplesSet, int epochsCount, double acceptableError, bool parallel)
         {
-            Debug.WriteLine("Обучение");
+            //Debug.WriteLine("Обучение");
             watch.Restart();
             double error = double.PositiveInfinity;
 
             for (int curEpoch = 0; curEpoch < epochsCount; ++curEpoch)
             {
-                Debug.WriteLine($"Эпоха {curEpoch}");
+                //Debug.WriteLine($"Эпоха {curEpoch}");
                 double errorSum = 0;
                 for (int i = 0; i < samplesSet.Count; ++i)
                 {
@@ -175,6 +144,7 @@ namespace NeuralNetwork1
             return error;
         }
 
+        //Ответ нейросети на входные данные
         protected override double[] Compute(double[] input)
         {
             //Передаем значениея сенсорам
@@ -188,30 +158,43 @@ namespace NeuralNetwork1
 
             return outputs.Select(x => x.output).ToArray();
         }
-        private void BackProp(Sample image, double ita)
+        //Обратное распространение ошибки
+        private void BackProp(Sample image)
         {
-            // Считываем ошибку из образа на выходной слой
-            for (int i = 0; i < outputs.Length; i++)
-                outputs[i].error = image.error[i];
-
-            // От выходов к корням
-            for (int i = layers.Length - 1; i >= 0; --i)
-                for (int j = 0; j < layers[i].Length; ++j)
-                    layers[i][j].BackpropError(ita);
-        }
-        
-        private double Sigmoid(double x) => 1.0 / (1.0 + Math.Exp(-x));
-        private double SigmoidDerivative(double x) => x * (1 - x);
-
-        private double GetError(double[] res, double[] expect) // Среднее квадратичное отклонение
-        {
-            double error = 0;
-            for (int i = 0; i < res.Length; i++)
+            //Ошибка выходного слоя
+            for (int i = 0; i < outputs.Length; ++i)
             {
-                error += Math.Pow(expect[i] - res[i], 2);
+                outputs[i].error = image.error[i];
             }
-            return error / 2;
-        }
 
+            // Рассчитываем ошибки для скрытых слоев
+            for (int layer = layers.Length - 2; layer > 0; --layer)
+            {
+                for (int j = 0; j < layers[layer].Length; ++j)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < layers[layer + 1].Length; ++k)
+                    {
+                        sum += layers[layer + 1][k].error * layers[layer + 1][k].weights[j];
+                    }
+                    layers[layer][j].error = sum * SigmoidDerivative(layers[layer][j].output);
+                }
+            }
+
+            // Обновляем веса для выходного слоя
+            for (int i = 0; i < outputs.Length; ++i)
+            {
+                outputs[i].AdjustWeights();
+            }
+
+            // Обновляем веса для скрытых слоев
+            for (int layer = layers.Length - 2; layer > 0; --layer)
+            {
+                for (int j = 0; j < layers[layer].Length; ++j)
+                {
+                    layers[layer][j].AdjustWeights();
+                }
+            }
+        }
     }
 }
